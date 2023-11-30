9516404cc6428c9480da7e666d7266d4bfb3bb18
jeffguzi <jeffreystephano@gmail.com>
2023-11-30 10:30:58 -0800

    adds story 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_29_231651) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_30_172415) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: :cascade do |t|
     t.string "name"
     t.string "scientific_binomial"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.integer "animal_id"
+    t.float "latitude"
+    t.float "longitude"
+    t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
